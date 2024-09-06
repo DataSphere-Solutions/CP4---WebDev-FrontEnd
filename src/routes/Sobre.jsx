e695aed5e8caef9a74450744ae3f78847aeb356e
Matheus <matheusestevao0309@gmail.com>
2024-09-06 20:32:08 -0300

    arrumando a fonte do sobre

--- src/routes/Sobre.jsx
+++ src/routes/Sobre.jsx
@@ -5,6 +5,7 @@ import { SobreStyle } from '../css/SobreStyle';
 const Sobre = () => {
     return (
         <SobreStyle>
+            <link href="https://fonts.googleapis.com/css2?family=Bungee&display=swap" rel="stylesheet"></link>
             <section className='classe'>
                 <h3 className='titulo '>ZONA FITNESS</h3>
                 <p className='texto'>ZONA FITNESS é uma empresa nacional de esportes voltada para venda de roupas esportivas. Atuamos principalemente com camisetas de times, sendo eles nacionais, internacionais ou até mesmo seleções. </p>
