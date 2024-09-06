d622048f838290d2bd69476c737d9882b4b9d066
Matheus <matheusestevao0309@gmail.com>
2024-09-06 20:50:50 -0300

    css da pagina produtos

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,36 +1,40 @@
-const Produtos=()=>{
-    return(
-        <section className="produtos-esportivos">
-  <div>
-    <img alt="Tênis esportivo" />
-    <h3>Tênis Esportivo</h3>
-    <p>
-      Confortável e resistente, ideal para corridas de longa distância. Feito com
-      materiais leves e tecnologia avançada para garantir o melhor desempenho em
-      suas atividades físicas.
-    </p>
-  </div>
+import { ProdutosStyle } from '../css/ProdutosStyle'; 
 
-  <div>
-    <img alt="Bola de Futebol" />
-    <h3>Bola de Futebol</h3>
-    <p>
-      Bola de futebol de alta qualidade, projetada para proporcionar controle
-      preciso e durabilidade em todas as condições. Perfeita para partidas
-      profissionais ou amadoras.
-    </p>
-  </div>
+const Produtos = () => {
+  return (
+    <ProdutosStyle>
+      <section className="produtos-esportivos">
+        <div>
+          <img src="tenis.jpg" alt="Tênis esportivo" />
+          <h3>Tênis Esportivo</h3>
+          <p>
+            Confortável e resistente, ideal para corridas de longa distância. Feito com
+            materiais leves e tecnologia avançada para garantir o melhor desempenho em
+            suas atividades físicas.
+          </p>
+        </div>
 
-  <div>
-    <img alt="Camisa de time" />
-    <h3>Camisa de time</h3>
-    <p>
-      Camisa extremamente leve , para praticar esportes e estar
-      na moda com seu time preferido
-    </p>
-  </div>
-</section>
+        <div>
+          <img src="bola.jpg" alt="Bola de Futebol" />
+          <h3>Bola de Futebol</h3>
+          <p>
+            Bola de futebol de alta qualidade, projetada para proporcionar controle
+            preciso e durabilidade em todas as condições. Perfeita para partidas
+            profissionais ou amadoras.
+          </p>
+        </div>
 
-    )
-}
-export default Produtos
+        <div>
+          <img src="camisa.jpg" alt="Camisa de time" />
+          <h3>Camisa de time</h3>
+          <p>
+            Camisa extremamente leve, para praticar esportes e estar na moda com seu
+            time preferido.
+          </p>
+        </div>
+      </section>
+    </ProdutosStyle>
+  );
+};
+
+export default Produtos;
